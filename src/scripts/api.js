7141c98673baaa0b64eaf5534975dee8c1193009
unknown <dakota.cookson1995@gmail.com>
2019-02-28 11:21:34 -0600

    a

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,23 +1,23 @@
 const API = {
 
     getData(resource) {
-        return fetch(`https://pipsocial.herokuapp.com/${resource}`)
+        return fetch(`https://pipsocial.herokuapp.com/api/${resource}`)
             .then(response => response.json())
     },
     getData3(resource) {
-        return fetch(`https://pipsocial.herokuapp.com/Stories/${resource}`)
+        return fetch(`https://pipsocial.herokuapp.com/api/Stories/${resource}`)
             .then(response => response.json())
     },
     getData2(resource) {
-        return fetch(`https://pipsocial.herokuapp.com/Stories?title_like=${resource}`)
+        return fetch(`https://pipsocial.herokuapp.com/api/Stories?title_like=${resource}`)
             .then(response => response.json())
     },
     getPayloadData(resource, payload) {
-        return fetch(`https://pipsocial.herokuapp.com/${resource}/${payload}`)
+        return fetch(`https://pipsocial.herokuapp.com/api/${resource}/${payload}`)
             .then(response => response.json())
     },
     postNewData(resource, payload) {
-        return fetch(`https://pipsocial.herokuapp.com/${resource}`, {
+        return fetch(`https://pipsocial.herokuapp.com/api/${resource}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -28,7 +28,7 @@ const API = {
 
     // put  fetch call passing in the paramaters Story id and story to edit wich are variables im converting the object Story to edit into a reavble json string
     putExistingStory(Storyid, StoryToEdit) {
-        return fetch(`https://pipsocial.herokuapp.com/Stories/${Storyid}`, {
+        return fetch(`https://pipsocial.herokuapp.com/api/Stories/${Storyid}`, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json"
@@ -37,7 +37,7 @@ const API = {
         })
     },
     deleteData(resource) {
-        return fetch(`https://pipsocial.herokuapp.com/Stories/${resource}`, {
+        return fetch(`https://pipsocial.herokuapp.com/api/Stories/${resource}`, {
             method: "DELETE",
             headers: {
                 "Content-Type": "application/json"
