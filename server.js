d7293c93b6f311b398b2fd8dd003579526ded9a1
zazitheangel1@gmail.com <zazitheangel1@gmail.com>
2019-04-06 11:29:37 -0500

    A

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const dbPath = './api/database.json'
+const dbPath = './api/database.json';
 const dbRoutes = Object.keys(require(dbPath)).map(r => '/' + r);
 const jsonServer = require('json-server');
 const server = jsonServer.create(dbRoutes);
 const router = jsonServer.router(dbPath);const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
-
 server.use(middlewares);
 server.use(router);
-
 server.listen(port);
